69c0beaaef71e8c5378bf35ee8db12000806b186
Tomichi <michna12@gmail.com>
2016-01-22 18:28:24 +0100

    submenu

--- main.cpp
+++ main.cpp
@@ -15,6 +15,17 @@ void printMainMenu() {
             "3 - Ukoncit program\n";
 }
 
+void printSubMenu() {
+    std::cout << "Vyberte volbu\n"
+            "1 - Zmenit pouze odpor R1 a vypocitat mezni frekvenci f0 s C1\n"
+            "2 - Zmenit pouze odpor R1 a vypocitat mezni frekvenci f0 s L1\n"
+            "3 - Zmenit pouze kondenzator C1 a vypocitat mezni frekvenci f0 s R1\n"
+            "4 - Zmenit pouze civku L1 a vypocitat mezni frekvenci f0 s R1\n"
+            "5 - Zmenit civku L1 a odpor R1 a vypocitat mezni frekvenci f0\n"
+            "6 - Zmenit kondenzator C1 a odpor R1 a vypocitat mezni frekvenci f0\n";
+            "7 - Pouzit vychozi hodnoty civky L1 a odporu R1 ze souboru a vypocitat mezni frekvenci f0\n";
+            "8 - Pouzit vychozi hodnoty kondenzatoru C1 a odporu R1 ze souboru a vypocitat mezni frekvenci f0\n";
+}
 
 int main(int argc, char *argv[]) {
     vector<Variable> variables;
@@ -46,11 +57,25 @@ int main(int argc, char *argv[]) {
         }
         switch (solution) {
             case 1 : {
-                cout << "Derivacni clanek\n";
+                std::cout << "Derivacni clanek\n";
+                int solutionDerivate;
+                printSubMenu();
+                std::cin >> solutionDerivate;
+                if (std::cin.fail()) {
+                    break;
+                }
+
+
                 break;
             }
             case 2 : {
-                cout << "Integracni clanek\n";
+                int solutionIntegrate;
+                std::cout << "Integracni clanek\n";
+                printSubMenu();
+                std::cin >> solutionIntegrate;
+                if (std::cin.fail()) {
+                    break;
+                }
                 break;
             }
 
