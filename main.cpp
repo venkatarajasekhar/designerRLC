f87507134ffa75bbebd293ac08b16f6ba88dfdf3
Tomichi <michna12@gmail.com>
2016-01-22 18:01:41 +0100

    Add basic menu

--- main.cpp
+++ main.cpp
@@ -8,16 +8,18 @@
 
 using namespace std;
 
+void printMainMenu() {
+    std::cout << "Vyberte kterou možnost chcete zvolit\n"
+            "1 - Navrhnout derivacni clanek\n"
+            "2 - Navrhnout integracni clanek\n"
+            "3 - Ukoncit program\n";
+}
+
+
 int main(int argc, char *argv[]) {
     vector<Variable> variables;
     initMathVariable(variables);
     vector<Expression> expression;
-
-    int **arrayFrequency = new int * [10];
-    initArray(arrayFrequency, 10);
-    printArray(arrayFrequency, 10);
-    deleteArray(arrayFrequency, 10);
-
 
     /* if (argc != 3) {
          cout << "Nevyhovuje seznam parametru programu.\n"
@@ -25,22 +27,56 @@ int main(int argc, char *argv[]) {
          return 1;
      }
  */
+
     loadVariables("/Users/tomichi/ClionProjects/designerRLC/variable.txt", variables);
-    cout << "tisk promomennych ";
-    printGlobalVariable(variables);
-
-    cout << searchVariableInVariablesStore(variables, "R1") << "\n" << searchVariableInVariablesStore(variables, "C2");
-
-    cout << "\n";
     loadExpressions("/Users/tomichi/ClionProjects/designerRLC/expression.txt", expression);
-    printGlobalExpressions(expression);
+    int **arrayFrequency = new int *[10];
+    initArray(arrayFrequency, 10);
+    //printArray(arrayFrequency, 10);
+    int solution;
+    bool stop = false;
+    while(1) {
+        if (stop) {
+            break;
+        }
+        printMainMenu();
+        std::cin >> solution;
+        if (std::cin.fail()) {
+            break;
+        }
+        switch (solution) {
+            case 1 : {
+                cout << "Derivacni clanek\n";
+                break;
+            }
+            case 2 : {
+                cout << "Integracni clanek\n";
+                break;
+            }
 
-    std::string pokus = createPolishNotation(expression.at(0).expression);
+            case 3 : {
+                stop = true;
+                break;
+            }
 
-    cout << polishNotationEval(pokus,variables) << "\n";
+            default : {
+                break;
+            }
+        }
+    }
+
+    //printGlobalVariable(variables);
+
+    //cout << searchVariableInVariablesStore(variables, "R1") << "\n" << searchVariableInVariablesStore(variables, "C2");
+
+    //printGlobalExpressions(expression);
+
+    //std::string pokus = createPolishNotation(expression.at(0).expression);
+
+    //cout << polishNotationEval(pokus,variables) << "\n";
 
     //parseExpression(expression[0].expression);
-
+    deleteArray(arrayFrequency, 10);
 
     return 0;
 }
